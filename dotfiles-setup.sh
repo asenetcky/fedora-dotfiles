d2c7cec2a1f2c3a2ff44060041acd899f247ce96
asenetcky <me@asenetcky.dev>
2025-12-30 06:48:04 -0500

    feat: update install scripts

---
+++ dotfiles-setup.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# Dotfiles setup
+
+echo "Backing up current .gitconfig"
+mv ~/.gitconfig ~/,gitconfig.bak
+
+echo "Backing up current .bashrc"
+mv ~/.bashrc ~/.bashrc.bak
+
+echo "stow dotfiles"
+cd ~/dotfiles
+stow .
