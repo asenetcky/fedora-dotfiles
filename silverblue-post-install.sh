19977e082deb69f942b6348e0a50b4f85bcb1c39
asenetcky <me@asenetcky.dev>
2025-12-30 15:09:07 -0500

    feat: add silverblue post install

---
+++ silverblue-post-install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Fedora Silverblue Post install
+
+### doco here: https://docs.fedoraproject.org/en-US/fedora-silverblue/tips-and-tricks/
+
+## Enabling RPM Fusions repos
+sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm && reboot
+
+## Major Fedora Updates
+sudo rpm-ostree update --uninstall rpmfusion-free-release --uninstall rpmfusion-nonfree-release --install rpmfusion-free-release --install rpmfusion-nonfree-release && reboot
